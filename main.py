d1b6dfd7250584819990987c91f747300dc6fff0
Ekrem Bakay <bakayekrem@gmail.com>
2021-04-25 15:56:44 +0200

    Reorganized the code and defined functions to better structure

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from tensorflow import keras
 from keras.layers import *
 from keras.models import *
@@ -9,6 +8,7 @@ from keras.utils import plot_model
 import os
 import numpy as np
 from keras.utils.vis_utils import plot_model
+
 
 def get_data(data_path, num_samples):
     input_texts = []
@@ -35,6 +35,89 @@ def get_data(data_path, num_samples):
 
     return input_characters, output_characters, input_texts, output_texts
 
+
+def prepare_data(max_encoder_seq_length, num_encoder_tokens, max_decoder_seq_length, num_decoder_tokens, input_texts, output_texts):
+    encoder_input_data = np.zeros(
+        (len(input_texts), max_encoder_seq_length, num_encoder_tokens), dtype="float32"
+    )
+
+    decoder_input_data = np.zeros(
+        (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
+    )
+
+    decoder_target_data = np.zeros(
+        (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
+    )
+
+    for i, (input_text, target_text) in enumerate(zip(input_texts, output_texts)):
+        for t, char in enumerate(input_text):
+            encoder_input_data[i, t, input_token_index[char]] = 1.0
+        encoder_input_data[i, t + 1:, input_token_index[" "]] = 1.0
+        for t, char in enumerate(target_text):
+            decoder_input_data[i, t, target_token_index[char]] = 1.0
+            if t > 0:
+                decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
+        decoder_input_data[i, t + 1:, target_token_index[" "]] = 1.0
+        decoder_target_data[i, t:, target_token_index[" "]] = 1.0
+
+    return encoder_input_data, decoder_input_data, decoder_target_data
+
+
+def compile_model(num_encoder_tokens, num_decoder_tokens, encoder_input_data, decoder_input_data,decoder_target_data):
+    encoder_inputs = keras.Input(shape=(None, num_encoder_tokens))
+    encoder = keras.layers.LSTM(latent_dim, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+
+    encoder_states = [state_h, state_c]
+
+    decoder_inputs = keras.Input(shape=(None, num_decoder_tokens))
+    decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = keras.layers.Dense(num_decoder_tokens, activation="softmax")
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+    model.compile(optimizer=Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.001), loss='categorical_crossentropy',
+                  metrics=["accuracy"])
+    model.fit(
+        [encoder_input_data, decoder_input_data],
+        decoder_target_data,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_split=0.2,
+    )
+    model.save("E2S")
+
+    return model
+
+def get_decoder(model):
+    encoder_inputs = model.input[0]  # input_1
+    encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
+    encoder_states = [state_h_enc, state_c_enc]
+    encoder_model = keras.Model(encoder_inputs, encoder_states)
+
+    decoder_inputs = model.input[1]  # input_2
+    decoder_state_input_h = keras.Input(shape=(latent_dim,), name="input_3")
+    decoder_state_input_c = keras.Input(shape=(latent_dim,), name="input_4")
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_lstm = model.layers[3]
+    decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+        decoder_inputs, initial_state=decoder_states_inputs
+    )
+    decoder_states = [state_h_dec, state_c_dec]
+    decoder_dense = model.layers[4]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = keras.Model(
+        [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
+    )
+
+    reverse_input_char_index = dict((i, char) for char, i in input_token_index.items())
+    reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
+
+    return encoder_model, decoder_model, reverse_input_char_index, reverse_target_char_index
+
+
 def decode_sequence(input_seq):
     states_value = encoder_model.predict(input_seq)
 
@@ -59,109 +142,35 @@ def decode_sequence(input_seq):
         states_value = [h, c]
     return decoded_sentence
 
+
 if __name__ == '__main__':
     n_features = 200 + 1
     batch_size = 64
     epochs = 200
     latent_dim = 256
     num_samples = 10000
-
     data_path = os.path.join(os.getcwd(), "Source")
 
-    input_characters, target_characters, input_texts, target_texts = get_data(data_path, num_samples)
+    input_characters, output_characters, input_texts, output_texts = get_data(data_path, num_samples)
 
     input_characters = sorted(list(input_characters))
-    target_characters = sorted(list(target_characters))
+    output_characters = sorted(list(output_characters))
     num_encoder_tokens = len(input_characters)
-    num_decoder_tokens = len(target_characters)
+    num_decoder_tokens = len(output_characters)
     max_encoder_seq_length = max([len(txt) for txt in input_texts])
-    max_decoder_seq_length = max([len(txt) for txt in target_texts])
-
-    print("No.of samples:", len(input_texts))
-    print("No.of unique input tokens:", num_encoder_tokens)
-    print("No.of unique output tokens:", num_decoder_tokens)
-    print("Maximum seq length for inputs:", max_encoder_seq_length)
-    print("Maximum seq length for outputs:", max_decoder_seq_length)
+    max_decoder_seq_length = max([len(txt) for txt in output_texts])
 
     input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
-    target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+    target_token_index = dict([(char, i) for i, char in enumerate(output_characters)])
 
-    print(input_token_index)
-    print(target_token_index)
+    encoder_input_data, decoder_input_data, decoder_target_data = \
+        prepare_data(max_encoder_seq_length, num_encoder_tokens, max_decoder_seq_length, num_decoder_tokens, input_texts, output_texts)
 
-    encoder_input_data = np.zeros(
-        (len(input_texts), max_encoder_seq_length, num_encoder_tokens), dtype="float32"
-    )
+    model = compile_model(num_encoder_tokens, num_decoder_tokens, encoder_input_data, decoder_input_data,decoder_target_data)
 
-    decoder_input_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
-    )
+    plot_model(model, to_file='modelsummary2.png', show_shapes=True, show_layer_names=True)
 
-    decoder_target_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
-    )
-
-    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-        for t, char in enumerate(input_text):
-            encoder_input_data[i, t, input_token_index[char]] = 1.0
-        encoder_input_data[i, t + 1:, input_token_index[" "]] = 1.0
-        for t, char in enumerate(target_text):
-            decoder_input_data[i, t, target_token_index[char]] = 1.0
-            if t > 0:
-                decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
-        decoder_input_data[i, t + 1:, target_token_index[" "]] = 1.0
-        decoder_target_data[i, t:, target_token_index[" "]] = 1.0
-
-    encoder_inputs = keras.Input(shape=(None, num_encoder_tokens))
-    encoder = keras.layers.LSTM(latent_dim, return_state=True)
-    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-
-    encoder_states = [state_h, state_c]
-
-    decoder_inputs = keras.Input(shape=(None, num_decoder_tokens))
-    decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
-    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-    decoder_dense = keras.layers.Dense(num_decoder_tokens, activation="softmax")
-    decoder_outputs = decoder_dense(decoder_outputs)
-
-    model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
-
-    model.compile(optimizer=Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.001), loss='categorical_crossentropy',
-                  metrics=["accuracy"])
-
-    model.fit(
-        [encoder_input_data, decoder_input_data],
-        decoder_target_data,
-        batch_size=batch_size,
-        epochs=epochs,
-        validation_split=0.2,
-    )
-    model.save("E2S")
-
-    plot_model(model, to_file='modelsummary.png', show_shapes=True, show_layer_names=True)
-
-    encoder_inputs = model.input[0]  # input_1
-    encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output  # lstm_1
-    encoder_states = [state_h_enc, state_c_enc]
-    encoder_model = keras.Model(encoder_inputs, encoder_states)
-
-    decoder_inputs = model.input[1]  # input_2
-    decoder_state_input_h = keras.Input(shape=(latent_dim,), name="input_3")
-    decoder_state_input_c = keras.Input(shape=(latent_dim,), name="input_4")
-    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-    decoder_lstm = model.layers[3]
-    decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
-        decoder_inputs, initial_state=decoder_states_inputs
-    )
-    decoder_states = [state_h_dec, state_c_dec]
-    decoder_dense = model.layers[4]
-    decoder_outputs = decoder_dense(decoder_outputs)
-    decoder_model = keras.Model(
-        [decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states
-    )
-
-    reverse_input_char_index = dict((i, char) for char, i in input_token_index.items())
-    reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
+    encoder_model, decoder_model, reverse_input_char_index, reverse_target_char_index = get_decoder(model)
 
     i = np.random.choice(len(input_texts))
     input_seq = encoder_input_data[i:i+1]
