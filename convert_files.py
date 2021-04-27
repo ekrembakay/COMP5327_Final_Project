6ad869015e76442d685dec2fbc13aede2aa78ce3
Ekrem Bakay <bakayekrem@gmail.com>
2021-04-27 21:40:01 +0200

    More input data added for better training

---
+++ convert_files.py
@@ -0,0 +1,21 @@
+import re
+import os
+def generate_files(input_path, filename):
+    output = ""
+    for file in os.listdir(input_path):
+        with open(os.path.join(input_path, file)) as fin:
+            string = ""
+            for line in fin:
+                string += re.sub('\s{2,4}', '$', line.strip("\n")) + "?"
+            output += "#" + string + "\n"
+
+    with open(filename, 'w') as fout:
+        fout.writelines(output)
+
+if __name__ == '__main__':
+
+    input_path = os.getcwd() + "/Input"
+    output_path = os.getcwd() + "/Output"
+
+    generate_files(input_path, "Input.txt")
+    generate_files(output_path, "Output.txt")
